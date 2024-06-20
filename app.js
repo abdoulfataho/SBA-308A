4e20ab45cd4dbc7f022fc2b203c02b3c046c44c2
abdoulfataho <yamyaogofataho@gmail.com>
2024-06-20 01:55:43 -0400

    data fetched from the dogapi

---
+++ app.js
@@ -0,0 +1,20 @@
+const apiKey = "live_p1tux0UtLsHaPmaXDoPemRfX9xM3FmfJ5xmo7QllBq7cokQR6tigxdBjF4Yylexv"
+const apiUrl = "https://api.thedogapi.com/v1/"
+        async function getDogImage() {
+            const breedName = document.getElementById('breedInput').value.trim();
+            if (!breedName) return;
+
+            try {
+                const response = await fetch(`https://api.thedogapi.com/v1/images/search?breed_ids=${breedName}`);
+                const data = await response.json();
+                const imageUrl = data[0].url;
+                const breed = data[0].breeds[0].name;
+                
+                document.getElementById('dogImage').src = imageUrl;
+                document.getElementById('dogBreed').textContent = `Breed: ${breed}`;
+            } catch (error) {
+                console.error('Error fetching dog image:', error);
+            }
+        }
+
+       
